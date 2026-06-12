baed0f6e0c773cfa7884ffe3b557ae6561e19a5c
luchi199 <umorenidy660@gmail.com>
2026-06-12 12:26:30 -0600

    Installed react-router and began working on functionality

--- fx-converter/src/App.tsx
+++ fx-converter/src/App.tsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+
 function App() {
-  return <h1>Hello world</h1>;
+  const [sendValue, setSendValue] = useState<string>("");
+  const [receiveValue, setReceiveValue] = useState<string>("");
+
+  function swapValues() {
+    const prev = sendValue;
+    setSendValue(receiveValue);
+    setReceiveValue(prev);
+  }
+
+  return (
+    <>
+      <h1>Hello world</h1>
+
+      <input
+        type="number"
+        value={sendValue}
+        onChange={(e) => setSendValue(e.target.value)}
+      />
+      <button type="button" onClick={swapValues}>
+        swap
+      </button>
+      <input
+        type="number"
+        value={receiveValue}
+        onChange={(e) => setReceiveValue(e.target.value)}
+      />
+    </>
+  );
 }
 
 export default App;
