69107deb366bf4ea1a948ffc4bc9b3e2e8258902
luchi199 <umorenidy660@gmail.com>
2026-06-14 20:51:27 -0600

    Started syncing on input and select elements

--- fx-converter/src/App.tsx
+++ fx-converter/src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { NavLink, Outlet } from "react-router";
 import { getCurrencies, convert } from "./services/api";
 import type { currenciesTypes } from "./services/api";
-import { useEffect } from "react";
 
 function App() {
   const [currencies, setCurrencies] = useState<Array<currenciesTypes>>([]);
+  const [sentCurrency, setSentCurrency] = useState<string>("USD");
+  const [recieveCurrency, setReceiveCurrency] = useState<string>("EUR");
+  const receiveInputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     async function displayCurrencies() {
@@ -15,14 +17,42 @@ function App() {
     displayCurrencies();
   }, []);
 
+  async function convertSend(sentInputValue?: string) {
+    let conversionValue;
+    if (sentInputValue) {
+      conversionValue = await convert(
+        sentCurrency,
+        recieveCurrency,
+        parseInt(sentInputValue),
+      );
+    }
+
+    if (conversionValue && receiveInputRef.current) {
+      console.log(conversionValue);
+      receiveInputRef.current.value = conversionValue.toString();
+    }
+  }
+
   return (
     <>
       <h1>Hello world</h1>
 
       <div>
-        <input name="sent" type="number" placeholder="0" />
+        <input
+          name="sent"
+          type="number"
+          placeholder="0"
+          onChange={async (e) => await convertSend(e.target.value)}
+        />
 
-        <select name="sent">
+        <select
+          name="sent"
+          defaultValue="USD"
+          onChange={(e) => {
+            setSentCurrency(e.target.value);
+            convertSend();
+          }}
+        >
           {currencies.map((currency: currenciesTypes) => (
             <option key={currency.iso_code} value={currency.iso_code}>
               {currency.iso_code} {currency.name}
@@ -34,8 +64,20 @@ function App() {
       <button type="button">swap</button>
 
       <div>
-        <input name="receive" type="number" placeholder="0" />
-        <select name="receive">
+        <input
+          ref={receiveInputRef}
+          name="receive"
+          type="number"
+          placeholder="0"
+        />
+        <select
+          name="receive"
+          defaultValue="EUR"
+          onChange={(e) => {
+            setReceiveCurrency(e.target.value);
+            convertSend();
+          }}
+        >
           {currencies.map((currency: currenciesTypes) => (
             <option key={currency.iso_code} value={currency.iso_code}>
               {currency.iso_code} {currency.name}
@@ -43,6 +85,8 @@ function App() {
           ))}
         </select>
       </div>
+
+      <hr />
 
       <NavLink to="/">History</NavLink>
       <NavLink to="/compare">Compare</NavLink>
--- fx-converter/src/services/api.ts
+++ fx-converter/src/services/api.ts
@@ -57,8 +57,8 @@ export async function convert(
     }
 
     const data = await res.json();
-    console.log(data);
-    return amount * data.rate;
+    const result: number = amount * data.rate;
+    return result;
   } catch (error) {
     console.error("Failed to convert values:", error);
   }
