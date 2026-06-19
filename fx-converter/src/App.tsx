8d6f14a281ec3145dd886546698ecd118d3559b4
luchi199 <umorenidy660@gmail.com>
2026-06-19 15:49:24 -0600

    Started restyling of conversion area

--- fx-converter/src/App.tsx
+++ fx-converter/src/App.tsx
@@ -4,6 +4,7 @@ import { getCurrencies, convert, getRates } from "./services/api";
 import type { currenciesTypes, ratesTypes } from "./services/api";
 import logo from "./assets/images/logo.svg";
 import exchange from "./assets/images/icon-exchange.svg";
+import chevron from "./assets/images/icon-chevron-down.svg";
 
 function App() {
   //#region
@@ -92,56 +93,106 @@ function App() {
           </div>
         </div>
       </header>
-
       <main className="flex-1 mx-auto md:p-6 px-4 py-8 md:py-12 w-full max-w-275">
-        <section className="flex flex-col gap-4">
+        <section className="flex flex-col gap-4 w-full">
           <h1 className="text-neutral-50 text-preset-2 uppercase">
             Check the rate
           </h1>
 
-          <div className="bg-neutral-700 shadow-[0_12px_40px_rgba(0,0,0,0.40)] rounded-[20px] w-full">
-            <div className="flex flex-wrap items-center gap-4 p-4 w-full">
-              <section className="flex flex-col flex-1 gap-5 bg-neutral-600 p-4 border border-neutral-500 rounded-2xl w-full h-max basis-xs">
-                <h2 className="text-neutral-100 text-preset-4 uppercase">
-                  send
-                </h2>
+          <div className="flex flex-col bg-neutral-700 shadow-[0_12px_40px_0_rgb(0,0,0,0.4)] rounded-[20px]">
+            {/* //////////////////// */}
+            <div className="flex flex-col items-center gap-4 p-4 border-neutral-500 border-b border-dashed w-full">
+              <div className="relative w-full">
+                <section className="flex flex-col gap-5 bg-neutral-600 p-4 border border-neutral-500 rounded-2xl w-full">
+                  <h2 className="text-neutral-100 text-preset-4 uppercase">
+                    send
+                  </h2>
 
-                <div className="flex justify-between w-full">
-                  <input
-                    name="sent"
-                    type="number"
-                    placeholder="0"
-                    value={sentInputValue}
-                    onChange={(e) => setSentInputValue(e.currentTarget.value)}
-                    className="focus:shadow-[0_0_0_4px_#cef739] rounded-lg outline-none w-30 text-neutral-50 text-preset-tablet-1 placeholder:text-neutral-200 decoration-1 decoration-neutral-200 not-focus:hover:underline underline-offset-3"
-                  />
-                  <select
-                    name="sent"
-                    value={sentSelectValue}
-                    onChange={(e) => setSentSelectValue(e.currentTarget.value)}
-                    className="w-full"
+                  <div className="flex justify-between items-center w-full">
+                    <input
+                      type="number"
+                      className="rounded-lg outline-none focus:ring-4 focus:ring-lime-500 w-fill max-w-30 text-neutral-50 text-preset-tablet-1 placeholder:text-neutral-200 decoration-1 decoration-neutral-200 not-focus:hover:underline underline-offset-4"
+                      placeholder="0"
+                    />
+
+                    <button className="flex justify-center items-center gap-2 bg-neutral-500 p-2.5 border border-neutral-400 rounded-lg">
+                      <p className="text-neutral-50 text-preset-4 uppercase">
+                        Click me
+                      </p>
+                      <img src={chevron} alt="" className="w-3" />
+                    </button>
+                  </div>
+                </section>
+                <div>
+                  <div>
+                    {/* <img src={} alt="search" /> */}
+                    <input type="text" placeholder="Search Currencies..." />
+                  </div>
+                  <ul
+                    data-open="closed"
+                    className="z-10 absolute bg-neutral-600 mt-2.5 p-2 border border-neutral-400 rounded-lg w-full h-125 overflow-y-scroll scrollbar-none"
                   >
-                    {currencies.map((currency: currenciesTypes) => (
-                      <option key={currency.iso_code} value={currency.iso_code}>
-                        {currency.iso_code} {currency.name}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              </section>
+                    <div className="flex justify-between items-center p-2 border-neutral-500 border-b text-neutral-200 text-preset-5">
+                      <p className="uppercase">popular</p>
+                      <p>3</p>
+                    </div>
 
-              <button className="bg-red-300 w-max">
-                <img src={exchange} alt="exchange icon" />
+                    <div className="flex justify-between items-center p-2 border-neutral-500 border-b text-neutral-200 text-preset-5">
+                      <p className="uppercase">other currencies</p>
+                      <p>{currencies.length - 3}</p>
+                    </div>
+
+                    {currencies.map((currency: currenciesTypes) => (
+                      <li
+                        data-value={currency.iso_code}
+                        key={currency.iso_code}
+                        value={currency.iso_code}
+                      >
+                        {currency.iso_code} {currency.name}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+
+              <button
+                onClick={swapValues}
+                className="flex justify-center items-center bg-neutral-600 border border-neutral-500 rounded-lg w-12 aspect-square"
+              >
+                <img
+                  src={exchange}
+                  alt="exchange"
+                  className="rotate-90 md:rotate-none"
+                />
               </button>
             </div>
+            {/* //////////////////// */}
+            <div>under</div>
+            {/* //////////////////// */}
           </div>
         </section>
       </main>
-
-      <button type="button" onClick={swapValues}>
-        swap
-      </button>
-
+      //#region
+      <div>
+        <input
+          name="sent"
+          type="number"
+          placeholder="0"
+          value={sentInputValue}
+          onChange={(e) => setSentInputValue(e.currentTarget.value)}
+        />
+        <select
+          name="sent"
+          value={sentSelectValue}
+          onChange={(e) => setSentSelectValue(e.currentTarget.value)}
+        >
+          {currencies.map((currency: currenciesTypes) => (
+            <option key={currency.iso_code} value={currency.iso_code}>
+              {currency.iso_code} {currency.name}
+            </option>
+          ))}
+        </select>
+      </div>
       <div>
         <input
           name="receive"
@@ -162,15 +213,13 @@ function App() {
           ))}
         </select>
       </div>
-
       <hr />
-
       <NavLink to="/">History</NavLink>
       <NavLink to="/compare">Compare</NavLink>
       <NavLink to="/favorites">Favorites</NavLink>
       <NavLink to="/log">Log</NavLink>
-
       <Outlet />
+      //#endregion
     </div>
   );
 }
